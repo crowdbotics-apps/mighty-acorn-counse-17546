502eb1bc5b22f7fc888486d94110903ca7328c59
Crowdbotics <team@crowdbotics.com>
2020-05-29 15:56:38 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/mighty_acorn_counse_17546/MainActivity.java
@@ -0,0 +1,15 @@
+package com.mighty_acorn_counse_17546;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "mighty_acorn_counse_17546";
+    }
+}
